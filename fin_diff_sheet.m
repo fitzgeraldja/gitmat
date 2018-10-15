e7ff57254585ab292d490b562ad435147554a088
John Fitzgerald <fitzgeraldj@maths.ox.ac.uk>
2018-10-15 14:12:24 +0100

    new file

---
+++ fin_diff_sheet.m
@@ -0,0 +1,14 @@
+N=100;
+vec1=ones(N,1);
+M=spdiags([vec1 -2*vec1 vec1],[-1 0 1], N, N);
+M(1,:)=zeros(1,N);
+M(1,1)=1;
+M(N,:)=zeros(1,N);
+M(N,N)=1;
+x=linspace(0,1,N)';
+f=10.*sin(20.*x)+cos(x.^5);
+f(1)=0;
+f(N)=0.1;
+u=M\f;
+figure(1);
+plot(x,u);
